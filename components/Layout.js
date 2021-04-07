a3362d53b69a22a11337710f472ff4e1564db5c9
Tetous <70217525+Tetous@users.noreply.github.com>
2021-04-07 10:41:47 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,15 +5,6 @@ const Layout = ({ children,title="Fast Anime" }) => {
     <div className="w-full justify-center items-center min-h-screen lg:h-full lg:w-10/12">
       <Head>
         <title>{title}</title>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GH8ZK8ZV56"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GH8ZK8ZV56');
-</script>
-
       </Head>
       {children}
     </div>
