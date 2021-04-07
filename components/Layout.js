753fdd5b3c58d624a59cc5ec2c615ff08113a8fb
Tetous <70217525+Tetous@users.noreply.github.com>
2021-04-07 10:23:42 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children,title="Fast Anime" }) => {
     <div className="w-full justify-center items-center min-h-screen lg:h-full lg:w-10/12">
       <Head>
         <title>{title}</title>
-    <meta name="google-site-verification" content="eMwVowoAMnT6_dVHIiA8F12nnffL8WH1HC_2X40Pgww" />
+<meta name="google-site-verification" content="eMwVowoAMnT6_dVHIiA8F12nnffL8WH1HC_2X40Pgww" />
       </Head>
       {children}
     </div>
